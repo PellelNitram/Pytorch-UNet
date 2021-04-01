03a1ca9075c19fbb03ec1428588cd95a31f42e4d
Martin Lellep <martin.lellep@physik.uni-marburg.de>
2021-04-01 08:17:45 +0100

    Add directories to CLI in training bash script.

--- run_training.sh
+++ run_training.sh
@@ -1,3 +1,6 @@
 python train.py \
     -e 5 \
-    -s 1.0
+    -s 1.0 \
+    -di /home/s1691089/myScratch/unet_training/maskRadius0.0/images/ \
+    -dm /home/s1691089/myScratch/unet_training/maskRadius0.0/masks/ \
+    -dc /home/s1691089/myScratch/unet_training/maskRadius0.0/checkpoints/
