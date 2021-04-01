d7a32cedc4271d0a5e12a53420362525eefab5b2
Martin Lellep <martin.lellep@physik.uni-marburg.de>
2021-04-01 17:07:43 +0100

    Tune learning rate in run_training script.

--- run_training.sh
+++ run_training.sh
@@ -1,6 +1,21 @@
-python train.py \
-    -e 5 \
-    -s 1.0 \
-    -di /home/s1691089/myScratch/unet_training/maskRadius0.0/images/ \
-    -dm /home/s1691089/myScratch/unet_training/maskRadius0.0/masks/ \
-    -dc /home/s1691089/myScratch/unet_training/maskRadius0.0/checkpoints/
+LEARNING_RATES=" 0.0001 0.001 0.00001 "
+LEARNING_RATES="0.0001"
+LEARNING_RATES="0.001"
+LEARNING_RATES="0.00001"
+
+for LEARNING_RATE in ${LEARNING_RATES}; do
+
+    echo ${LEARNING_RATE}
+
+    OUTFOLDER=/home/s1691089/myScratch/unet_training/maskRadius0.0/checkpoints/lr${LEARNING_RATE}/
+    mkdir -p ${OUTFOLDER}
+
+    python train.py \
+        -e 5 \
+        -s 1.0 \
+        -l ${LEARNING_RATE} \
+        -di /home/s1691089/myScratch/unet_training/maskRadius0.0/images/train/ \
+        -dm /home/s1691089/myScratch/unet_training/maskRadius0.0/masks/train/ \
+        -dc ${OUTFOLDER}
+
+done
