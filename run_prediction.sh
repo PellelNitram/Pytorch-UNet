0b79e7abba30349217b04f301277f956111d7da0
Martin Lellep <martin.lellep@physik.uni-marburg.de>
2021-04-02 09:06:34 +0100

    Extend run_prediction script.

--- run_prediction.sh
+++ run_prediction.sh
@@ -1,11 +1,23 @@
+LEARNING_RATES="0.0001 0.001 0.00001"
 BASE_FOLDER='/home/s1691089/myScratch/unet_training/maskRadius0.0'
 
-MODEL_CHECKPOINT=${BASE_FOLDER}/checkpoints/CP_epoch5.pth
-SAMPLE_IMAGE=${BASE_FOLDER}/images/image.0.png
-OUTFOLDER=${BASE_FOLDER}/predictions
+for LEARNING_RATE in ${LEARNING_RATES}; do
 
-python predict.py \
-    -i ${SAMPLE_IMAGE} \
-    -o ${OUTFOLDER}/test_prediction.png \
-    --model ${MODEL_CHECKPOINT}
+    echo ${LEARNING_RATE}
 
+    REPLACEMENT=predictions/lr${LEARNING_RATE}
+    TO_REPLACE=images/test
+
+    MODEL_CHECKPOINT=${BASE_FOLDER}/checkpoints/lr${LEARNING_RATE}/CP_epoch5.pth
+    SAMPLE_IMAGES=$(find ${BASE_FOLDER}/images/test -maxdepth 1 -type f)
+    OUTFOLDER=${BASE_FOLDER}/predictions/lr${LEARNING_RATE}
+    OUTFILES="${SAMPLE_IMAGES//$TO_REPLACE/$REPLACEMENT}"
+
+    mkdir -p ${OUTFOLDER}
+
+    python predict.py \
+        -i ${SAMPLE_IMAGES} \
+        -o ${OUTFILES} \
+        --model ${MODEL_CHECKPOINT}
+
+done
